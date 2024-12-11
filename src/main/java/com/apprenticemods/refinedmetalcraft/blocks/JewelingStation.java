e42884fbe5c744ff8d4cf806ddfe8bc792c3dad3
davenonymous <david.hilberath@gmx.de>
2024-12-11 14:04:25 +0100

    Add item model and voxel shape to Jeweling Station

--- src/main/java/com/apprenticemods/refinedmetalcraft/blocks/JewelingStation.java
+++ src/main/java/com/apprenticemods/refinedmetalcraft/blocks/JewelingStation.java
@@ -1,15 +1,20 @@
 package com.apprenticemods.refinedmetalcraft.blocks;
 
-import com.apprenticemods.refinedmetalcraft.RefinedMetalCraft;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class JewelingStation extends Block {
+	public static final VoxelShape SHAPE = Shapes.box(0, 0, 0, 1, 0.875, 1);
 
 	public JewelingStation(Properties properties) {
 		super(properties);
@@ -32,5 +37,25 @@ public class JewelingStation extends Block {
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
 		super.createBlockStateDefinition(builder);
 		builder.add(BlockStateProperties.FACING);
+	}
+
+	@Override
+	protected VoxelShape getVisualShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	protected VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	protected VoxelShape getOcclusionShape(BlockState state, BlockGetter level, BlockPos pos) {
+		return SHAPE;
+	}
+
+	@Override
+	protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+		return SHAPE;
 	}
 }
