24fabce22c87fa9a7e75f0ece92b73e5e081ae6e
davenonymous <david.hilberath@gmx.de>
2024-12-14 15:02:26 +0100

    Save and sync inventories

--- src/main/java/com/apprenticemods/refinedmetalcraft/blocks/JewelingStationEntity.java
+++ src/main/java/com/apprenticemods/refinedmetalcraft/blocks/JewelingStationEntity.java
@@ -3,9 +3,16 @@ package com.apprenticemods.refinedmetalcraft.blocks;
 import com.apprenticemods.refinedmetalcraft.setup.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.items.ItemStackHandler;
+import org.jetbrains.annotations.Nullable;
 
 public class JewelingStationEntity extends BlockEntity {
 	public ItemStackHandler outputInventoryHandler;
@@ -26,11 +33,20 @@ public class JewelingStationEntity extends BlockEntity {
 
 	}
 
+	public void notifyClients(boolean all) {
+		if (this.level != null) {
+			if (!this.level.isClientSide()) {
+				this.level.sendBlockUpdated(this.worldPosition, this.level.getBlockState(this.worldPosition), this.level.getBlockState(this.worldPosition), all ? 3 : 2);
+			}
+		}
+	}
+
 	private ItemStackHandler createOutputInventory() {
 		return new ItemStackHandler(1) {
 			@Override
 			protected void onContentsChanged(int slot) {
 				setChanged();
+				notifyClients(false);
 			}
 		};
 	}
@@ -42,6 +58,7 @@ public class JewelingStationEntity extends BlockEntity {
 			@Override
 			protected void onContentsChanged(int slot) {
 				setChanged();
+				notifyClients(false);
 			}
 
 			@Override
@@ -60,4 +77,52 @@ public class JewelingStationEntity extends BlockEntity {
 	public void redstonePulse() {
 
 	}
+
+	private void loadFirstStackFromTag(String label, ItemStackHandler handler, CompoundTag tag, HolderLookup.Provider registries) {
+		if(tag.contains(label)) {
+			handler.setStackInSlot(0, ItemStack.parse(registries, tag.getCompound(label)).orElse(ItemStack.EMPTY));
+		}
+	}
+
+	@Override
+	protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+		super.loadAdditional(tag, registries);
+		loadFirstStackFromTag("output", outputInventoryHandler, tag, registries);
+		loadFirstStackFromTag("front", frontInventoryHandler, tag, registries);
+		loadFirstStackFromTag("right", rightInventoryHandler, tag, registries);
+		loadFirstStackFromTag("back", backInventoryHandler, tag, registries);
+		loadFirstStackFromTag("left", leftInventoryHandler, tag, registries);
+	}
+
+	private CompoundTag saveFirstStackToTag(String label, ItemStackHandler handler, CompoundTag tag, HolderLookup.Provider registries) {
+		var stack = handler.getStackInSlot(0);
+		if(!stack.isEmpty()) {
+			tag.put(label, stack.save(registries, new CompoundTag()));
+		}
+		return tag;
+	}
+
+	@Override
+	protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+		super.saveAdditional(tag, registries);
+		saveFirstStackToTag("output", outputInventoryHandler, tag, registries);
+		saveFirstStackToTag("front", frontInventoryHandler, tag, registries);
+		saveFirstStackToTag("right", rightInventoryHandler, tag, registries);
+		saveFirstStackToTag("back", backInventoryHandler, tag, registries);
+		saveFirstStackToTag("left", leftInventoryHandler, tag, registries);
+	}
+
+	@Override
+	public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+		CompoundTag tag = super.getUpdateTag(registries);
+		saveAdditional(tag, registries);
+		return tag;
+	}
+
+	@Override
+	public @Nullable Packet<ClientGamePacketListener> getUpdatePacket() {
+		return ClientboundBlockEntityDataPacket.create(this);
+	}
+
+
 }
